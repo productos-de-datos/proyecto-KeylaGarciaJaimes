f11bb87f73f44bb57334e2f2bd96907ec4d95acd
KeylaGarciaJaimes <kjgarciaj@unal.edu.co>
2022-06-28 21:24:28 -0500

    pipeline

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -12,11 +12,74 @@ En luigi llame las funciones que ya creo.
 
 """
 
-if __name__ == "__main__":
+import ingest_data
+import transform_data
+import clean_data
+import compute_daily_prices
+import compute_monthly_prices
+import luigi
+from luigi import Task, LocalTarget
 
-    raise NotImplementedError("Implementar esta función")
+class IngestData(Task):
+    def output(self):
+        return LocalTarget("data_lake/landing/rsl.txt")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            ingest_data.ingest_data()
+
+class TransformData(Task):
+    def requires(self):
+        return IngestData()
+    
+    def output(self):
+        return LocalTarget("data_lake/raw/rsl2.txt")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            transform_data.transform_data()
+
+class CleanData(Task):
+    def requires(self):
+        return TransformData()
+
+    def output(self):
+        return LocalTarget("data_lake/cleansed/rsl3.txt")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            clean_data.clean_data()
+
+class DailyPrices(Task):
+    def requires(self):
+        return CleanData()
+
+    def output(self):
+        return LocalTarget("data_lake/business/precios-dia.csv")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            compute_daily_prices.compute_daily_prices()
+
+class MontlyPrices(Task):
+    def requires(self):
+        return CleanData()
+
+    def output(self):
+        return LocalTarget("data_lake/business/precios-mes.csv")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            compute_monthly_prices.compute_monthly_prices()
+
+
+class PreciosPromedioMensual(Task):
+    def requires(self):
+        return [DailyPrices(), MontlyPrices()]
+
 
 if __name__ == "__main__":
     import doctest
+    luigi.run(["PreciosPromedioMensual", "--local-scheduler"])
 
     doctest.testmod()
