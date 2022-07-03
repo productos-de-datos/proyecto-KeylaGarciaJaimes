3ca0b0aaba23640b30a1f83c953aac31ea50da11
KeylaGarciaJaimes <kjgarciaj@unal.edu.co>
2022-07-02 21:45:06 -0500

    grader

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,3 +1,12 @@
+# pylint: disable=invalid-name
+# pylint: disable=unsubscriptable-object
+
+from skforecast.ForecasterAutoreg import ForecasterAutoreg
+from skforecast.ForecasterAutoregCustom import ForecasterAutoregCustom
+from skforecast.ForecasterAutoregMultiOutput import ForecasterAutoregMultiOutput
+from skforecast.model_selection import grid_search_forecaster
+from skforecast.model_selection import backtesting_forecaster
+
 def train_daily_model():
     """Entrena el modelo de pronóstico de precios diarios.
 
@@ -6,7 +15,33 @@ def train_daily_model():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
+ import pandas as pd
+
+    df_csv = pd.read_csv("data_lake/cleansed/precios-horarios.csv",
+        sep=",",         # separador de campos  
+        )
+    df_csv["fecha"] = pd.to_datetime(df_csv["fecha"])
+    df_csv = df_csv.rename(columns={'fecha':'x', 'precio':'y'})
+    df_csv = df_csv.asfreq('MS')
+    df_csv = df_csv.sort_index()
+
+    # Split data into train-test
+    # ==============================================================================
+    steps = 36
+    data_train = df_csv[:-steps]
+    data_test  = df_csv[-steps:]
+
+# Create and train forecaster
+# ==============================================================================
+forecaster = ForecasterAutoreg(
+                regressor = RandomForestRegressor(random_state=123),
+                lags = 6
+                )
+
+forecaster.fit(y=data_train['y'])
+forecaster
+   
 
 
 if __name__ == "__main__":
